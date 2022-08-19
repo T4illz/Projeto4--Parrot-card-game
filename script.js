999a2755ea80cb8403c6e21ffe68eaf849f3a0b2
Rodrigo Goncalves Colhado <Digow.colhado@gmail.com>
2022-08-18 23:34:26 -0300

    Arrumado escolhas de cartas entre 4 e 14

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ let qtdCartas = prompt('Com quantas cartas você quer jogar?');
 function QuantidadeJgs(){
     const lista = document.querySelector('.guarda-cartas');
     lista.innerHTML = '';
-    if( qtdCartas <= 4 || qtdCartas >= 14){
+    const par = qtdCartas % 2;
+    console.log(par);
+
+    if (par === 0 && qtdCartas >= 4 && qtdCartas <= 14){
+        for (let i = 0; i < qtdCartas; i++){
+            lista.innerHTML = lista.innerHTML + `
+            <div class="carta-jogo">
+            <img src="imgs/foxies.jpg" />
+        </div>
+        `}
+        } else if (par !== 0){
+            alert('Por favor selecionar um numero par de cartas!');
+            qtdCartas = prompt('Com quantas cartas você quer jogar?');
+            QuantidadeJgs();
+        } else if( qtdCartas < 4 || qtdCartas >= 14){
         alert('O minimo de cartas são 4 e o maximo são 14');
-    }
-    else if (qtdCartas >= 4 || qtdCartas <= 14){
-    for (let i = 0; i < qtdCartas; i++){
-        lista.innerHTML = lista.innerHTML + `
-        <div class="carta-jogo">
-        <img src="imgs/foxies.jpg" />
-    </div>
-    `}
-}
+        }
+    
 
 }
 QuantidadeJgs();
+
+
