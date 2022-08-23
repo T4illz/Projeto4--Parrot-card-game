8d33d34fc2a80980537ba786b5a2e195009640cb
Rodrigo Goncalves Colhado <Digow.colhado@gmail.com>
2022-08-23 15:35:54 -0300

    Versão arrumada(pós livecoding) só para pegar o conteudo

--- script.js
+++ script.js
@@ -1,150 +1,181 @@
-let qtdCartas = prompt('Com quantas cartas você quer jogar?');
+let qtdCartas;
+let carta1, carta2;
 let contador = 0;
-let cartas = [`
-        <div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-            <img src="imgs/MaisTails.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Raid.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tails-dançando.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-            <img src="imgs/Tails-Follow.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tailz-exe.gif" />
-        </div>`,
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tailz.gif" />
-        </div>`,
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Voando.gif" />
-        </div>`,
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/MaisTails.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Raid.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tails-dançando.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-            <img src="imgs/Tails-Follow.gif" />
-        </div>`, 
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tailz-exe.gif" />
-        </div>`,
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Tailz.gif" />
-        </div>`,
-        `<div onclick="virarCartas(this)" class="carta-jogo carta-virada">
-        <img src="imgs/Voando.gif" />
-        </div>`
-    ];
+
+const imgCartas = [
+    `MaisTails`, 
+    `Raid`,
+    `Tails-dançando`,
+    `Tails-Follow`,
+    `Tailz-exe`,
+    `Tailz`,
+    `Voando`
+];
+
+const baralho = [];
+
 let pares = 0;
-cartas.sort(randomizando); 
+
 
 function randomizando(){
 
     return Math.random() - 0.5;
 
 }
-randomizando();
 
-        /* div padrão
-    <div class="carta-jogo">
-        <img src="imgs/Tailz.jpg" />
-    </div>
-*/
+
+function jogo(){
+
+    for (let i = 0; i < (qtdCartas/2); i++){
+        let carta = imgCartas[i];
+
+        baralho.push(carta);
+        baralho.push(carta);
+    }
+
+    baralho.sort(randomizando);
+
+}
+
 
 function QuantidadeJgs(){
 
-    const lista = document.querySelector('.guarda-cartas');
+    qtdCartas = Number(prompt('Com quantas cartas você quer jogar?'));
 
-    lista.innerHTML = '';
+    while (verificando()){
+        qtdCartas = Number(prompt('Com quantas cartas você quer jogar?'));
+    }
 
-    const par = qtdCartas % 2;
-    
-    if (par === 0 && qtdCartas >= 4 && qtdCartas <= 14){
+    gerandoBaralho();
 
-        for (let i = 0; i < qtdCartas; i++){
-            
-            lista.innerHTML = lista.innerHTML + cartas[i];
-            
-    
-        }
-        } else if (par !== 0){
-
-            alert('Por favor selecionar um numero par de cartas!');
-
-            qtdCartas = prompt('Com quantas cartas você quer jogar?');
-
-            QuantidadeJgs();
-
-        } else if( qtdCartas < 4 || qtdCartas >= 14){
-
-        alert('O minimo de cartas são 4 e o maximo são 14');
-
-        qtdCartas = prompt('Com quantas cartas você quer jogar?');
-
-        QuantidadeJgs();
-
-        }
 }
 
 QuantidadeJgs();
 
-let verificaCartas = '';
+function verificando(){
+
+    if (isNaN(qtdCartas)){
+        
+        alert('Por favor digitar um numero');
+        return true;
+    } 
+    if (qtdCartas < 4 || qtdCartas >14){
+
+        alert('O minimo de cartas são 4 e o maximo são 14');
+        return true;
+    } 
+    if (qtdCartas % 2 !== 0){
+
+        alert('Por favor selecionar um numero par de cartas!');
+        return true;        
+    }
+
+    return false;
+
+}
+
+
+
+function gerandoBaralho(){
+    
+    for (let i = 0; i < (qtdCartas/2); i++){
+    let carta = imgCartas[i];
+    baralho.push(carta);
+    baralho.push(carta);
+    }
+
+    baralho.sort(randomizando);
+
+    montarBaralho();
+
+}
+
+function montarBaralho(){
+    const mesa = document.querySelector('.guarda-cartas');
+
+    for (let i = 0; i < baralho.length; i++){
+        mesa.innerHTML += `
+        <div class="carta-jogo" onclick="virarCartas(this)">
+            <div class="carta-jogo">
+                <div class="frente carta">
+                    <img src="imgs/foxies.jpg" />
+                </div>
+                <div class="costas carta">
+                    <img src="imgs/${baralho[i]}.gif" />
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+}
+
 
 function virarCartas(clicada){
-    contador++; // contagem de jogadas.
+     // contagem de jogadas.
     
-    clicada.classList.toggle('carta-virada'); // desvira a carta
-    
-    console.log(clicada);
-    console.log(verificaCartas);
-
-    if (clicada.innerHTML === verificaCartas ){
-        
-        clicada.classList.add('carta-par');
-        verificaCartas = '';
-        console.log(clicada);
-        pares++;
-        finalJogo();
-        return
+    if (clicada.classList.contains('virada')){
+        return;
     }
-        verificaCartas = clicada.innerHTML;
-    
-        console.log(verificaCartas);
-    
-    setTimeout(voltaCartas, 5000);
+
+    if (carta1 !== undefined && carta2 !== undefined){
+        return;
+    }
+
+    if (carta1 === undefined){
+        carta1 = clicada;
+        carta1.classList.add('virada');
+        contador++;
+    } else {
+        if (carta2 === undefined){
+            carta2 = clicada;
+            carta2.classList.add('virada');
+            contador++;
+
+            if (carta1.innerHTML === carta2.innerHTML){
+                //Acertou.
+                console.log('acertou!')
+                pares++;
+
+                redefinir();
+                
+                setTimeout(finalJogo, 1000);
+                
+            } else {
+                console.log('errou!')
+                    //errou
+                    setTimeout(voltaCartas, 1000);
+            }
+        }
+    }
+  
 }
 
 function voltaCartas(){
 
-    const voltar = document.querySelector('.guarda-cartas .carta-jogo');
-    if (voltar !== null){
-    voltar.classList.add('carta-virada');
-    verificaCartas = '';
-    }
-}
-// se a carta for igual 
-// adicionar class '.carta-par' e retirar 'carta-virada'
-// se não, não fazer nada.
+    carta1.classList.remove('virada');
+    carta2.classList.remove('virada');
+    
+    redefinir();
 
+}
+function redefinir(){
+
+    carta1 = undefined;
+    carta2 = undefined;
+}
 
 function finalJogo(){
-    if(pares === 7){
+  
+    if ((pares * 2) === qtdCartas){
+           
     alert(`Você ganhou em ${contador} jogadas!`);
-    const reiniciar = prompt('Deseja reiniciar o jogo?');
+    const reiniciar = confirm('Deseja jogar novamente?');
+    
+    if (reiniciar){
+        window.location.reload();
     }
-    if (reinciar === 'sim'){
-        qtdCartas = prompt('Com quantas cartas você quer jogar?');
-        QuantidadeJgs();
-    }
+}
 }
 
 
